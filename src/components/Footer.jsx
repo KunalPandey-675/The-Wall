354379a94fe9d80c1469a7655ee86fc0cb35d605
Kunal Pandey <pandeykunal2084@gmail.com>
2025-05-12 18:34:45 +0530

    Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
-          <span className="mb-3 mb-md-0 text-body-secondary">
-            © 2025 Company, Inc
+      <footer className="d-flex justify-centre align-items-center py-3 my-4 border-top footer">
+        <div className="col-md-4 d-flex align-items-center ">
+          <span className="mb-3 mb-md-0 text-body-primary">
+            Made With ❤️ by Kunal Pandey
           </span>
         </div>
       </footer>
