760d638556f4bbadff02af48f6c0d83114748306
Kunal Pandey <pandeykunal2084@gmail.com>
2025-08-16 19:16:55 +0530

    server fix

--- frontend/src/store/PostStore.js
+++ frontend/src/store/PostStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand"
 import axios from "axios";
 
 const BASE_URL = process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/api"
+    ? "https://the-wall-backend.onrender.com/api"
     : "/api";
 
 // Configure axios to include cookies with requests
